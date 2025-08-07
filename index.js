108d1df8a4f02f4eae7c6f3b5ef7e8d66ca522c0
hmodateresyc <36571234+hmodateresyc@users.noreply.github.com>
2025-08-07 14:15:01 +0200

    removed Global Variables

--- index.js
+++ index.js
@@ -3,5 +3,10 @@
 const NikoHomeControlPlatform = require('./lib/NikoHomeControlPlatform')
 
 module.exports = function (homebridge) {
-  homebridge.registerPlatform('homebridge-NikoHomeControl', 'NikoHomeControl', NikoHomeControlPlatform, true)
+  homebridge.registerPlatform(
+    'homebridge-NikoHomeControl',
+    'NikoHomeControl',
+    NikoHomeControlPlatform,
+    true
+  )
 }
